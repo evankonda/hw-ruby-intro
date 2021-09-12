5aece5a532004344b740e6c515106da7cbab357e
EC2 Default User <ec2-user@ip-172-31-39-186.us-east-2.compute.internal>
2021-09-12 03:45:08 +0000

    Part 1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -9,15 +9,34 @@ def sum arr
     sum += i
   end
   return sum
-  
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    maxvals = arr.max(2)
+    return maxvals[0] + maxvals[1]
+  end
 end
 
-def sum_to_n? arr, n
+def sum_to_n arr, n
   # YOUR CODE HERE
+  if arr.length <= 1
+    return false
+  else
+    for i in 0..(arr.length - 2)
+      for j in (i + 1)..(arr.length - 1)
+        if arr[i] + arr[j] == n
+          return true
+        end
+      end
+    end
+    return false
+  end
 end
 
 # Part 2
@@ -39,3 +58,6 @@ end
 class BookInStock
 # YOUR CODE HERE
 end
+
+print sum_to_n?([1, 2, 3], 5)
+
